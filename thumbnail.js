f2779569ac7d0c7e73ab4cd6cfd0762f272a9e72
sltdjtl <100214077+sltdjtl@users.noreply.github.com>
2022-02-26 19:44:10 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   //let targetImage = document.querySelector("#smart-image");
+   let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
     alert("点击了图片");
 });
